1e5133c9aa0f48d474e6c670a3435510b334b7fa
BigieCheese <ppajerski@cps.edu>
2022-12-01 15:23:32 -0600

    updated todo

--- src/core/Screen.java
+++ src/core/Screen.java
@@ -6,44 +6,34 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
-public class Screen extends JFrame{
+public class Screen extends JFrame {
 	public Dimension prefSize;
-	
-	
+
 	// the main panel that will intercept all keystrokes and mouse data
 	// contains all menus and panels on it
-	public JPanel GUIlayer; 
-	
-	// space for the simulation to be drawn 
+	public JPanel GUIlayer;
+
+	// space for the simulation to be drawn
 	// *WARNING: may dynamically resize, aspect ratio not guaranteed*
 	public JPanel simSpace;
-	
+
 	public JMenuBar topMenu;
-	
+
 	public Screen() {
 		super();
-		
-		
+		GUIlayer = new JPanel();
+		simSpace = new JPanel();
+	}
+	
+	public void create() {
 		GUIlayer.add(simSpace);
 		this.add(GUIlayer);
 		this.setPreferredSize(prefSize);
 		this.setContentPane(GUIlayer);
 		
-		
-		
-		
-		
-		
+		this.setVisible(true);
 	}
-	
-	public void addPanes() {}
-	
-	public void show() {this.setVisible(true);}
-	
-	
-	
-	
-	
+
 	
 
 }
--- src/core/main.java
+++ src/core/main.java
@@ -2,12 +2,21 @@ package core;
 
 public class main {
 
+	public static Screen screen;
+	
+	
 	public static void main(String[] args) {
 		
 		//welcome to the bio game. a lot of work lies ahead
 		
 		
 		
+		
+		//TODO load prefs
+		//TODO load 'projects'
+		//TODO load current simulation (start a simulator)
+		//creates the main screen, visible
+		screen = new Screen();
 		
 		
 		
